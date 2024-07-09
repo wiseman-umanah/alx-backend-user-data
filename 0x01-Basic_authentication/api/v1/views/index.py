c2ff42f0c653161cb7194299cd195f6b68852544
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-09 16:00:34 +0100

    Added function to handle basic auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauth() -> str:
+    """For testing unauthorization
+    Return:
+      - error
+    """
+    abort(401)
