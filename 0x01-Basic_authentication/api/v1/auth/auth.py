db171fb97c908a36ad0b5d192779de408a2092d5
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-10 04:49:05 +0100

    Handled Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,6 +37,8 @@ class Auth:
         Returns:
             str: returns None
         """
+        if request is not None:
+            return request.headers.get("Authorization", None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
