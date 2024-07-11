4e0053a3448deeecd5221a0f29cc1f41ff6c08ab
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-11 23:04:21 +0100

    Added a func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -35,3 +35,14 @@ class SessionAuth(Auth):
         """
         session_cookies = self.session_cookie(request)
         return User.get(self.user_id_for_session_id(session_cookies))
+
+    def destroy_session(self, request=None):
+        """Destroys an authenticated session.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if (request is None or session_id is None) or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+        return True
