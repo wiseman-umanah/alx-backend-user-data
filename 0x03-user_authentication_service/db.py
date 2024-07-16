8728581c0832884dab587cb6aa5a353e16c9b2aa
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-16 15:29:14 +0100

    Fixed pycodestyle error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,12 +29,12 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user to db
 
         Args:
-            email (str): email of user 
+            email (str): email of user
             hashed_password (str): hashed password of user
 
         Returns:
