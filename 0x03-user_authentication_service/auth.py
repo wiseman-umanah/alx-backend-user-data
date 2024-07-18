4f48bf35a49180a0f482af8b8ecba7bbe531685e
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-18 19:37:33 +0100

    Correctly annotated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bcrypt.hashpw:
@@ -67,7 +68,7 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
         """Finds user object based on the session_id
         """
         try:
